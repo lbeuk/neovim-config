9ba3f3777ce2c6c04932902da9c2a0dd0d5f484e
Luke Beukelman <lbeuk@pm.me>
2024-03-05 19:05:51 -0700

    Initialized neovim config

---
+++ init.lua
@@ -0,0 +1,49 @@
+-- Vim preferences
+vim.wo.number = true
+
+-- Bootstrap lazy.nvim (https://github.com/folke/lazy.nvim)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load lazy.nvim
+require("lazy").setup({
+	"neovim/nvim-lspconfig",
+})
+
+local lspconfig = require'lspconfig'
+
+local on_attach = function(client)
+	require'completion'.on_attach(client)
+end
+
+lspconfig.rust_analyzer.setup({
+	on_attach = on_attach,
+	settings = {
+		["rust-analyzer"] = {
+			imports = {
+				granularity = {
+					group = "module",
+				},
+				prefix = "self",
+			},
+			cargo = {
+				buildScripts = {
+					enable = true,
+				},
+			},
+			procMacro = {
+				enable = true,
+			},
+		}
+	}
+})

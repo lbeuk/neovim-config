72b701a9a8e2fa8779b6b6eb60b54af805d35608
Luke Beukelman <lbeuk@pm.me>
2024-03-24 12:23:14 -0600

    Changes

--- init.lua
+++ init.lua
@@ -72,10 +72,16 @@ require("lazy").setup({
 		"folke/todo-comments.nvim",
 		lazy=false,
 		dependencies = {"nvim-lua/plenary.nvim"}
+	},
+	-- Better status line
+	{
+    		'nvim-lualine/lualine.nvim',
+    		dependencies = { 'nvim-tree/nvim-web-devicons' }
 	}
 })
 
 require("todo-comments").setup()
+require("lualine").setup()
 
 ------------------
 -- Auto closing --
@@ -145,8 +151,48 @@ vim.wo.number = true
 vim.b.expandtab = true
 vim.cmd[[colorscheme tokyonight]]
 
----------------------
--- Vim Keybindings --
----------------------
+-------------------
+-- Key remapping --
+-------------------
 
-vim.keymap.set("n", "<C-b>", "<Cmd>Neotree toggle<CR>") -- Ctrl+b :: Toggles/selects filesystem viewer
+-- Ctrl+b :: Toggles/selects filesystem viewer
+vim.keymap.set("n", "<C-b>", "<Cmd>Neotree toggle<CR>")
+
+-- LSP capbilities
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
